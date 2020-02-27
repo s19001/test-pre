9cc9ead85c83b5ca5b70f4779a262f146cb8f20a
s19001 <s19001@std.it-college.ac.jp>
2020-02-27 09:06:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Card,
+  CardHeader,
+  CardActions,
+  CardContent,
+  Button,
+  TextField
+} from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props) 
+    this.state = {
+      locate: '',
+      zip_code: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    fetch('https://api.zipaddress.net/?zipcode=' + this.state.zip_code, {
+      mode: 'cors'
+    })
+      .then(res => {
+        return res.json()
+      })
+      .then(json => {
+        this.setState({ locate: json.data.fullAddress })
+      })
+  }
+
+  render () {
+    return (
+      <Card>
+        <CardHeader title="郵便番号から住所を検索" />
+        <CardActions>
+          <TextField
+            type='number'
+            value={this.state.zip_code}
+            onChange={e => this.setState({ zip_code: e.target.value })}
+          />
+          <Button variant="outlined" onClick={this.handleClick} color="primary">
+            住所を検索
+          </Button>
+        </CardActions>
+        <CardContent>
+          <p>住所: {this.state.locate}</p>
+        </CardContent>
+      </Card>
+    )
+  }
 }
 
 export default App;
