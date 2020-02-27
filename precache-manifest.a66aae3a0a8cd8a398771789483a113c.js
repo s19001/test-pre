eabae8dcf8dbc349f1338cb1f0afe83820bd6b05
s19001 <s19001@std.it-college.ac.jp>
2020-02-27 14:40:55 +0900

    Updates

---
+++ precache-manifest.a66aae3a0a8cd8a398771789483a113c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efcde25f31fd854286ff00aaf3f1124",
+    "url": "/test-pre/index.html"
+  },
+  {
+    "revision": "fa71c4ab7014f5332a02",
+    "url": "/test-pre/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "363e6be269399624819a",
+    "url": "/test-pre/static/js/2.16468143.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/test-pre/static/js/2.16468143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa71c4ab7014f5332a02",
+    "url": "/test-pre/static/js/main.15829348.chunk.js"
+  },
+  {
+    "revision": "e9ee27c445c717a8fc2b",
+    "url": "/test-pre/static/js/runtime-main.4a3db8b8.js"
+  }
+]);
